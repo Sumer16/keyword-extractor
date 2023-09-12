51f0f41284c1cedec23358f353efd107776508a9
Sumer Patel <patelsum@oregonstate.edu>
2023-09-11 22:27:26 -0700

    Added logic for keyword extraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// React
+import { useState } from 'react' // Can also use Redux
+
 // Chakra UI
 import { Container, Box } from '@chakra-ui/react'
 
@@ -7,9 +10,36 @@ import Footer from './components/Footer'
 import TextInput from './components/TextInput'
 
 const App = () => {
+  const [ keywords, setKeywords ] = useState([])
+  const [ isOpen, setIsOpen ] = useState(false)
+  const [ loading, setLoading ] = useState(false)
 
-  const extractKeywords = (text) => {
-    console.log(text)
+  const extractKeywords = async (text) => {
+    setLoading(true)
+    setIsOpen(true)
+
+    const options = { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}` // To import VITE ENV
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate them with commas \n\n' + text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8
+      })
+    }
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options)
+    const json = response.json()
+    const data = json.choices[0].text.trim()
+
+    console.log(data)
+    setKeywords(data)
+    setLoading(false)
   }
 
   return (
