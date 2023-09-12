68b1c5acc401dcf6fcb2d53016a5403a3d0bb567
Sumer Patel <patelsum@oregonstate.edu>
2023-09-11 17:39:27 -0700

    Added base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import { Container, Box } from '@chakra-ui/react'
+
 const App = () => {
   return (
-    <>
-      <h1>Keyword Extractor</h1>
-    </>
+    <Box bg='blue.600' color='white' height='100vh' paddingTop='130px'>
+      <Container maxWidth='3xl' centerContent>
+        <h1>Keyword Extractor</h1>
+      </Container>
+    </Box>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
