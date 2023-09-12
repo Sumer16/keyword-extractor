c1a44eae5008f212e038ec103a40e1df4c4aa0bf
Sumer Patel <patelsum@oregonstate.edu>
2023-09-11 19:55:30 -0700

    Added TextInput

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import { Container, Box } from '@chakra-ui/react'
 // Components
 import Header from './components/Header'
 import Footer from './components/Footer'
+import TextInput from './components/TextInput'
 
 const App = () => {
+
+  const extractKeywords = (text) => {
+    console.log(text)
+  }
+
   return (
     <Box bg='teal.600' color='white' height='100vh' paddingTop='130px'>
       <Container maxWidth='3xl' centerContent>
         <Header />
+        <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
     </Box>
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,51 @@
+// React
+import { useState } from 'react'
+
+// Chakra UI
+import { Textarea, Button, useToast } from '@chakra-ui/react'
+
+const TextInput = ({ extractKeywords }) => {
+  const [ text, setText ] = useState('')
+
+  const toast = useToast()
+
+  const submitText = () => {
+    if (text === '') {
+      toast({
+        title: 'Text field is empty',
+        description: 'Please enter some text to extract keywords',
+        status: 'error',
+        duration: 5000,
+        isClosable: false
+      })
+    } else {
+      extractKeywords(text)
+    }
+  }
+
+  return (
+    <>
+      <Textarea 
+        bg='teal.400' 
+        color='white' 
+        padding={4} 
+        marginTop={6} 
+        height={200} 
+        value={text} 
+        onChange={ (e) => setText(e.target.value) } 
+      />
+      <Button 
+        bg='teal.500' 
+        color='500' 
+        marginTop={4} 
+        width='100%' 
+        _hover={{ bg: 'teal.700' }} 
+        onClick={submitText}
+      >
+        Extract Keywords
+      </Button>
+    </>
+  )
+}
+
+export default TextInput
