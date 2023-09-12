e8feb889316cf283248736cfd47bff1c493d9aa2
Sumer Patel <patelsum@oregonstate.edu>
2023-09-11 23:47:59 -0700

    Added Modal for keyword extractor

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Container, Box } from '@chakra-ui/react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import TextInput from './components/TextInput'
+import KeywordModal from './components/KeywordsModal'
 
 const App = () => {
   const [ keywords, setKeywords ] = useState([])
@@ -29,18 +30,26 @@ const App = () => {
         prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate them with commas \n\n' + text + '',
         temperature: 0.5,
         max_tokens: 60,
-        frequency_penalty: 0.8
+        frequency_penalty: 0.8,
+        top_p: 1.0,
+        presence_penalty: 0.0,
       })
     }
 
-    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options)
-    const json = response.json()
-    const data = json.choices[0].text.trim()
-
-    console.log(data)
-    setKeywords(data)
-    setLoading(false)
+    try {
+      const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options)
+      const json = response.json()
+      const data = json.choices[0].text.trim()
+      setKeywords(data)
+      setLoading(false)
+    } catch (err) {
+      console.log(err)
+    }
   }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
     <Box bg='teal.600' color='white' height='100vh' paddingTop='130px'>
@@ -49,6 +58,12 @@ const App = () => {
         <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   )
 }
---
+++ src/components/KeywordsModal.jsx
@@ -0,0 +1,41 @@
+// Chakra UI
+import {
+  Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  CircularProgress,
+} from '@chakra-ui/react'
+
+const KeywordsModal = ({ keywords, loading, isOpen, closeModal }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Keywords</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display='flex' alignItems='center' justifyContent='center'>
+            {loading ? (
+              <CircularProgress isIndeterminate color='teal.300' />
+            ) : (
+              <Text>{keywords}</Text>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='teal' mr={3} onClick={closeModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default KeywordsModal
